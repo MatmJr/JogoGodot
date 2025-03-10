a75fec7eeb415cd44b3ced75817f7ce6a8087d32
Marco Mialaret Júnior <89661320+MatmJr@users.noreply.github.com>
2025-03-09 23:19:54 -0300

    Criação do hitbox

--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,14 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
+var currentHealth = 3
+
+func _on_hurt_box_area_entered(area):
+	if area.name == "hitBox":
+		currentHealth -= 1
+		print_debug(currentHealth)
+		if currentHealth <= 0:
+			get_tree().reload_current_scene()
 
 func _physics_process(delta: float) -> void:
 
