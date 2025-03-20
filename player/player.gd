a1257a7193808f7ed856143207153bed10dec669
Marco Mialaret Júnior <89661320+MatmJr@users.noreply.github.com>
2025-03-19 21:41:03 -0300

    Código da aula 7

--- player/player.gd
+++ player/player.gd
@@ -5,6 +5,8 @@ const SPEED = 300.0
 
 @export var maxHealth = 3
 @onready var currentHealth = maxHealth
+@onready var animation = $AnimationPlayer
+@onready var weapon = $PlayerWeapon
 
 func _on_hurt_box_area_entered(area):
 	if area.name == "hitBox":
@@ -15,7 +17,6 @@ func _on_hurt_box_area_entered(area):
 			get_tree().reload_current_scene()
 
 func _physics_process(delta: float) -> void:
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -30,16 +31,38 @@ func _physics_process(delta: float) -> void:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	move_and_slide()
 
+func _ready() -> void:
+	weapon.visible = false
 
 @onready var WalkAnimation = $AnimatedSprite2D
 func _process(delta):
 	if Input.is_action_pressed("ui_down"):
 		WalkAnimation.play("down")
+		if Input.is_action_pressed("attack"):
+			animation.play("attackDown")
+			weapon.visible = true
+		else:
+			weapon.visible = false
 	elif Input.is_action_pressed("ui_up"):
 		WalkAnimation.play("up")
+		if Input.is_action_pressed("attack"):
+			animation.play("attackUp")
+			weapon.visible = true
+		else:
+			weapon.visible = false
 	elif Input.is_action_pressed("ui_right"):
 		WalkAnimation.play("right")
+		if Input.is_action_pressed("attack"):
+			animation.play("attackRight")
+			weapon.visible = true
+		else:
+			weapon.visible = false
 	elif Input.is_action_pressed("ui_left"):
 		WalkAnimation.play("left")
+		if Input.is_action_pressed("attack"):
+			animation.play("attackLeft")
+			weapon.visible = true
+		else:
+			weapon.visible = false
 	else:
 		WalkAnimation.stop()
